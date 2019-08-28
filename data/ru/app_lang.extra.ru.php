367186d884c462a3147f2021a07048076234adc7
Anatoly P <c3la-ns@protonmail.com>
2019-08-29 00:57:36 +0100

    ver 1.8.0

---
+++ data/ru/app_lang.extra.ru.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['APP_ACCESS_DENIED'] = '<span style=\"color:red\">ДОСТУП ЗАПРЕЩЕН</span>. Вы должны сначала авторизоваться, чтобы читать и отправлять сообщения в чате.';
+$lang['USERNAME_BANNED'] = 'Пользователь заблокирован';
+$lang['USERNAME_PASSWORD_NOT_RECOGNIZED'] = 'Неправильные имя и/или пароль';
+$lang['USERNAME_TAKEN'] = 'Имя занято';
+$lang['REGISTRATION_COMPLETE'] = 'Регистрация завершена';
