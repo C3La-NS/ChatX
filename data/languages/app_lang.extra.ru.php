869bfdf08b1f581a14fd46bb66652bb34a5937e0
Anatoly P <c3la-ns@protonmail.com>
2019-08-29 00:55:55 +0100

    Delete app_lang.extra.ru.php

--- data/languages/app_lang.extra.ru.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$lang['APP_ACCESS_DENIED'] = '<span style=\"color:red\">ДОСТУП ЗАПРЕЩЕН</span>. Вы не вошли в аккаунт или используете приложение с неавторизованного ресурса.';
-$lang['USERNAME_BANNED'] = 'Пользователь заблокирован';
-$lang['USERNAME_PASSWORD_NOT_RECOGNIZED'] = 'Неправильные имя и/или пароль';
