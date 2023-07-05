d8246dfbda1d1f6fe5b941834a41a34b7e6926ef
Anatoly P <c3la-ns@protonmail.com>
2023-07-06 02:09:26 +0300

    Delete data directory

--- data/languages/en/demo_page.en.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$lang = array();
-
-$lang['title'] = 'Chat Demo-page. You may want to turn it off in settings.';
-$lang['expandChat'] = 'Expand chat from tray';
-$lang['moveChat'] = 'Move widget by pressing & holding left mouse button on \"Chat\" title';
-$lang['resizeChat'] = 'Resize chat height: move cursor a bit before reply-box. Then drag the slider';
-$lang['adminPanel'] = 'Moderation panel';
--- data/languages/ru/app_lang.extra.ru.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['APP_ACCESS_DENIED'] = '<span style=\"color:var(--chx-11-color)\">ДОСТУП ЗАПРЕЩЕН</span>. Вы должны сначала авторизоваться, чтобы читать и отправлять сообщения в чате.';
-$lang['USERNAME_BANNED'] = 'Пользователь заблокирован';
-$lang['USERNAME_PASSWORD_NOT_RECOGNIZED'] = 'Неправильные имя и/или пароль';
-$lang['USERNAME_TAKEN'] = 'Имя занято';
-$lang['REGISTRATION_COMPLETE'] = 'Регистрация завершена';
